1227cd8b21dafdf237f2c293d7096dd84460471d
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-31 23:04:03 +0300

    Add option model

---
+++ AdvancedQuestionGenerator.Web/Models/Option.cs
@@ -0,0 +1,13 @@
+﻿using AdvancedQuestionGenerator.Web.Models.Base;
+
+namespace AdvancedQuestionGenerator.Web.Models
+{
+    public class Option : BaseEntity
+    {
+        public string Title { get; set; }
+        bool Status { get; set; }
+
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+    }
+}
