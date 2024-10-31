95800dbc1f2198e49b46754f26f6932123a77a5b
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-31 23:03:56 +0300

    Add exam model

---
+++ AdvancedQuestionGenerator.Web/Models/Exam.cs
@@ -0,0 +1,9 @@
+﻿using AdvancedQuestionGenerator.Web.Models.Base;
+
+namespace AdvancedQuestionGenerator.Web.Models
+{
+    public class Exam : BaseEntity
+    {
+        public string Title { get; set; }
+    }
+}
