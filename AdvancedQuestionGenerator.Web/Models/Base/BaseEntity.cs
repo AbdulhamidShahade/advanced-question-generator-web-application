03168159a7706b6dd98b85035401b8edab4e84a3
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-31 23:03:32 +0300

    Add BaseEntity concrete class

---
+++ AdvancedQuestionGenerator.Web/Models/Base/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿
+namespace AdvancedQuestionGenerator.Web.Models.Base
+{
+    public class BaseEntity : IBaseEntity
+    {
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime CreatedAt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime UpdatedAt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
