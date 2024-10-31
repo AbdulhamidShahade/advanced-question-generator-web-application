bbb37ece07a9ac77421a1d06d100ad86c0b9ec5e
Abdulhamid <shahade.abdulhamid@gmail.com>
2024-10-31 23:03:49 +0300

    Add category model

---
+++ AdvancedQuestionGenerator.Web/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using AdvancedQuestionGenerator.Web.Models.Base;
+
+namespace AdvancedQuestionGenerator.Web.Models
+{
+    public class Category : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public List<Question> Questions { get; set; }
+    }
+}
